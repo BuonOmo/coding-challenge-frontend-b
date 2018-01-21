ef2d28f986ef06618562a805430e99686e15f36d
BuonOmo <buonomo.ulysse@gmail.com>
2018-01-21 20:05:01 +0100

    add first css for SearchForm

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,21 +1,38 @@
 import React from 'react'
+import {Button, Col, ControlLabel, Form, FormGroup, FormControl} from 'react-bootstrap'
 
 const SearchForm = ({from, to, date, onChange, onSubmit}) => (
-	<form>
-		<label>
-			From
-			<input type="text" value={from} name="fromInput" onChange={e => onChange('from', e.target.value)}/>
-		</label>
-		<label>
-			To
-			<input type="text" value={to} name="toInput" onChange={e => onChange('to', e.target.value)}/>
-		</label>
-		<label>
-			When
-			<input type="date" value={new Date(date).toLocaleDateString('en-CA')} name="dateInput" onChange={e => onChange('date', new Date(e.target.value))}/>
-		</label>
-		<input type="submit" onClick={onSubmit} value="search"/>
-	</form>
+	<Form horizontal>
+		<FormGroup controlId="fromInput">
+			<Col componentClass={ControlLabel} sm={2}>From</Col>
+			<Col sm={10}><FormControl
+				type="text"
+				value={from}
+				onChange={e => onChange('from', e.target.value)}
+			/></Col>
+		</FormGroup>
+		<FormGroup controlId="toInput">
+			<Col componentClass={ControlLabel} sm={2}>To</Col>
+			<Col sm={10}><FormControl
+				type="text"
+				value={to}
+				onChange={e => onChange('to', e.target.value)}
+			/></Col>
+		</FormGroup>
+		<FormGroup controlId="dateInput">
+			<Col componentClass={ControlLabel} sm={2}>When</Col>
+			<Col sm={10}><FormControl
+				type="text"
+				value={new Date(date).toLocaleDateString('en-CA')}
+				onChange={e => onChange('date', new Date(e.target.value))}
+			/></Col>
+		</FormGroup>
+		<FormGroup>
+			<Col smOffset={2} sm={10}>
+				<Button type="submit" onClick={onSubmit}>Search</Button>
+			</Col>
+		</FormGroup>
+	</Form>
 )
 
 export default SearchForm
